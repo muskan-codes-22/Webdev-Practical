d7ca7f351a9f036a2d03e5d5d78561b64b099a83
muskan-codes-22 <musukumari2211@gmail.com>
2026-05-07 10:58:52 +0530

    feat: update button text for clarity and comment out unused user fetching code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div>
       <h1>Like Button</h1>
       <button onClick={() => setLiked(!liked)}>{liked ? "❤️" : "🤍"}</button>
-      <p>You liked the button</p>
+      <p>You liked the button then its red</p>
     </div>
   );
 }
@@ -15,17 +15,17 @@ function App() {
 export default App;
 
 
-import {useState,useEffect} from "react";
-function App() {
-  const [users, setUsers] = useState([]);
+// import {useState,useEffect} from "react";
+// function App() {
+//   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setUsers(data));
-  }, []);
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((data) => setUsers(data));
+//   }, []);
 
 
 
-}
-export default App;
+// }
+// export default App;
